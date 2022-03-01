2b922c8408a212cda54606b882906c5310d3d302
Ankit-cg22 <ankitdas.cg22@gmail.com>
2022-03-01 22:58:39 +0530

    minor changes

--- src/components/TotalBudgetCard.js
+++ src/components/TotalBudgetCard.js
@@ -4,7 +4,7 @@ import { useBudgetContext } from '../contexts/BudgetContext'
 
 export default function TotalBudgetCard() {
     const {expenses, budgets} = useBudgetContext();
-
+    if(budgets.length == 0)return null;
     const amount = expenses.reduce((total , expense) => total + expense.amount , 0);
     const limit = budgets.reduce((total , budget) => total + budget.max , 0)  ;
   return (
