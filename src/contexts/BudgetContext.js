6bfe4cab123bfffd1b01b389d377b4d1ba955ec4
Ankit-cg22 <ankitdas.cg22@gmail.com>
2022-03-01 16:48:01 +0530

    useContext() hook

--- src/contexts/BudgetContext.js
+++ src/contexts/BudgetContext.js
@@ -1,4 +1,5 @@
-import React , {useContext} from 'react'
+import React , {useContext , useState} from 'react'
+import {v4 as uuidV4} from 'uuid'
 
 const BudgetContext = React.createContext()
 
@@ -6,9 +7,57 @@ export function useBudgetContext(){
     return useContext(BudgetContext)
 }
 
+// budget {
+//     id : 
+//     name :
+//     max:
+// }
+
+//expense{
+//     id:
+//     budgetID:
+//     amount:
+//     description:
+// }
+
 export const BudgetProvider = ({children}) =>{
+    const [budgets , setBudgets] = useState([]) 
+    const [expenses , setExpenses] = useState([]) 
+
+    function getBudgetExpenses(budgetID){
+        return expenses.filter(expense => expense.budgetID === budgetID)
+    }
+    function addExpense(){
+
+    }
+    function addBudget({name , max}){
+        
+        setBudgets(prevBudgets => {
+
+            if(prevBudgets.find(budget => budget.name === name)) {
+                return prevBudgets;
+            }
+
+            return [...prevBudgets , { id : uuidV4() , name , max}]
+        })
+    }
+    function deleteBudget(){
+
+    }
+    function deleteExpense(){
+
+    }
+    
     return (
-        <BudgetContext.Provider value={{}}>
+        <BudgetContext.Provider value={{
+            budgets,
+            expenses,
+            getBudgetExpenses,
+            addExpense,
+            addBudget,
+            deleteBudget,
+            deleteExpense
+        }}>
             {children}
         </BudgetContext.Provider>
     )
